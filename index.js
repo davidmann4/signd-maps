f1aa3d6633a44f314d3d1057239bbcf4191939ba
David Mann <daaavid.mann@gmail.com>
2023-08-21 11:53:06 +0200

    make it available to public

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ $( document ).ready(function(){
 
     let menu= $("<div></div>").attr("id","menu");
 
-    menu.append(menu_item("KYB Coverage","img/kyb-coverage.png").addClass("selected"));
-    menu.append(menu_item("KYC Coverage","img/kycid-coverage.png"));
-    menu.append(menu_item("Open Banking Coverage","img/open-banking-coverage.png"));
-    menu.append(menu_item("eKYC Coverage","img/ekyc-coverage.png"));
-    menu.append(menu_item("NFC Coverage","img/nfc-document-coverage.png"));
+    menu.append(menu_item("KYB Coverage","https://davidmann4.github.io/signd-maps/img/kyb-coverage.png").addClass("selected"));
+    menu.append(menu_item("KYC Coverage","https://davidmann4.github.io/signd-maps/img/kycid-coverage.png"));
+    menu.append(menu_item("Open Banking Coverage","https://davidmann4.github.io/signd-maps/img/open-banking-coverage.png"));
+    menu.append(menu_item("eKYC Coverage","https://davidmann4.github.io/signd-maps/img/ekyc-coverage.png"));
+    menu.append(menu_item("NFC Coverage","https://davidmann4.github.io/signd-maps/img/nfc-document-coverage.png"));
 
-    let content =  $("<div></div>").attr("id","signd-map-content").append($("<img></img>").attr("src","img/kyb-coverage.png"));
+    let content =  $("<div></div>").attr("id","signd-map-content").append($("<img></img>").attr("src","https://davidmann4.github.io/signd-maps/img/kyb-coverage.png"));
 
 
     $("#signd-map").append(menu);
